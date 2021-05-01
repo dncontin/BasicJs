c8c09c2b60fce4f5ff167f615e6a2289516a5c1b
Diego Contin <dncontin@gmail.com>
2021-05-01 15:23:36 -0300

    primera parte del uso de java script

---
+++ app.js
@@ -0,0 +1,52 @@
+// Variables
+var x = 5;
+var y = 6;
+var z = x +y;
+
+console.log("El valor es : "+z);
+
+
+//Identificadores
+/*
+  Nombre a 1 _ $
+  comenzar con una letra
+  pueden comenzar ocn $ o _
+  senibles a may min
+  no podemos usar como nombre una palabra reservada
+*/
+
+
+// Tipos de datos
+//podemos usar la misma variable para != tipos de daots
+
+var lenght = 16;
+var lastname = "Pepe";
+var x = {firstName: "Pepe", lastName: "Argento"}; //object
+
+var a; //undefined
+a=5;
+console.log(a);
+
+a= "Pepe";
+console.log(a);
+
+//comillas
+/*
+''
+´´
+""
+*/
+
+var carName1 = "Volvo XC60";
+var carName2 = 'Volvo XC60';
+
+var answer1 = "It's alright";
+var answer2 = "He is called 'Fernando'";
+var answer3 = 'He is called "Francisco"';
+
+
+console.log(carName1);
+console.log(carName2);
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
